a88adf82a0bad1d4a03c0270b1b9e5ed22da63e9
Bruno Lima <brunofigueiredolima@gmail.com>
2024-12-13 12:37:07 -0300

    login_view implemented

--- netFitApp/views.py
+++ netFitApp/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import *
-# Create your views here.
 
 def index(request):
     return  render(request, "netFitApp/index.html", {
         "mensagem": 'testando'
     })
+
+def login_view(request):
+    if request.method == "POST":
+        # recebe os atributos e loga
+        username = request.POST['username']
+        password = request.POST['senha']
+        user = authenticate(request, username=username, password=password)
+
+        # checa resultado
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("criarTreino"))
+        else:
+            return render(request, "netFitApp/login.html", {
+                "msg_erro": "Username/senha inválidos"
+            })
+    else:
+        return render(request, "netFitApp/login.html")
+    
+def register(request):
+    pass
+
