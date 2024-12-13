9e74bf2db37769aaa58ae235e462230e8b47cc5b
Gabriel-A-Araujo <antunes.gabriel.345@gmail.com>
2024-12-13 15:11:42 -0300

    Correção de bugs nos formulários

--- netFitApp/forms.py
+++ netFitApp/forms.py
@@ -3,20 +3,21 @@ from django.forms.models import inlineformset_factory
 from .models import Treino, Serie
 
 # Formulário usado para um cliente criar um novo treino
-class formTreino(forms.modelForm):
+class TreinoForm(forms.ModelForm):
     class Meta:
         model = Treino
-        fields = ['Nome', 'Gasto calórico']
+        fields = ['nome', 'gastoCalorico']
 
         widgets = {
-            'Nome': forms.TextInput(attrs={'class': 'form-control'}),
-            'Gasto calórico': forms.TextInput(attrs={'class': 'form-control'}),
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'gastoCalórico': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
 # inLineFormSet para associar uma lista de séries ao formulário de treino
 SerieInlineFormSet = inlineformset_factory(
     Treino,  # Modelo pai
     Serie,  # Modelo filho
+    fields=['n_repeticoes', 'n_series', 'exercicio'],
     extra=3,  # Número de formulários vazios exibidos
     can_delete=True  # Permite excluir séries
 )
